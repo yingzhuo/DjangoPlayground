6bdd173db8edcecfdc9b6b634193cdfd9d5fc07f
应卓 <yingzhor@gmail.com>
2022-09-03 08:53:48 +0800

    update

--- common/security.py
+++ common/security.py
@@ -13,10 +13,9 @@ from pathlib import Path
 from django_sugar import web, lang
 from rest_framework import permissions, throttling
 
-JWT_SECRET_KEY = web.create_rs256_algorithm(
+JWT_SECRET_KEY = web.create_es256k_algorithm(
     public_key=lang.read_file_as_bytes(Path(__file__).parent / 'rsa.public'),
     private_key=lang.read_file_as_bytes(Path(__file__).parent / 'rsa.private'),
-    passphrase='DjangoPlayground',
 )
 
 
